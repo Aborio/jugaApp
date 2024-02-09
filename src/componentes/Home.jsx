9e7ec880ed3fb05ca134b4b3be49a25ff9a11aa4
Aborio <aguborio18@gmail.com>
2024-02-09 14:25:07 -0400

    push

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -9,9 +9,6 @@ const Home = () => {
     return (
         <div className='titulo'> 
             <h1 className='titulo'>Bienvenid@ a Ases Del Norte</h1>
-            <div className='imcon'>
-            <img src='/casino2.jpg' alt='Casino' className='imagen'></img>
-            </div>
             <br />
             <button className='boton' >
              <a className='boton2' href='https://wa.link/4476ir'>Click aqui para contactarte con nosotros</a>
